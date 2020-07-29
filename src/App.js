4d1d739e0ec5f5919d5e3ffa02858984a029a07f
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2020-07-29 18:47:10 -0500

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      color: '',
+      clickCounter: ''
+    }
+  }
+
+
+
+
   render() {
     return(
       <div>
