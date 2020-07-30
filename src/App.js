e0293d5750ddf0aa082d04db7c47ee73d361783d
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2020-07-30 00:19:02 -0500

    update

--- src/App.js
+++ src/App.js
@@ -4,19 +4,38 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props)
+    //Setting state properties.
     this.state = {
-      color: '',
-      clickCounter: ''
-    }
+      color: 'orange',
+      clickCounter: 0
+    };
+
+  
   }
+  //creating the increment function.
+    increment = () => {
+      this.setState({clickCounter: this.state.clickCounter +1});
+    };
+
+    onChangeHandler = () => {
+      let newColor = 'blue';
+      if(this.state.color === newColor){
+      this.setState({color: this.state.newColor});
+      }
+    }
+    
 
 
 
 
   render() {
+    const styleColor = {
+      background: this.state.color
+    };
     return(
       <div>
-
+        <div style={styleColor}>{this.state.clickCounter}</div>
+            <button onChange={this.onChangeHandler} value={this.state.color} onClick ={this.increment} > click me</button>
       </div>
     );
   }
