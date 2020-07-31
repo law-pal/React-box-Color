c483b9ab70a3180df02926a0af58399c05c20463
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2020-07-30 22:36:39 -0500

    update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       clickCounter: 0
     };
 }
-  // function increment.
+  
     increment = () => {
       this.setState({clickCounter: this.state.clickCounter +1});
     };
